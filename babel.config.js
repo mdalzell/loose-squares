fef74a72b840365255c118677f483f6375f8ebe9
mdalzell <matt.a.dalzell@gmail.com>
2019-04-27 18:36:52 -0500

    Fixed linting warnings

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      "babel-preset-expo",
-      "module:metro-react-native-babel-preset",
-      "module:react-native-dotenv"
-    ]
+      'babel-preset-expo',
+      'module:metro-react-native-babel-preset',
+      'module:react-native-dotenv',
+    ],
   };
 };
