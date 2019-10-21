621478a77b6258c0f86ab502136e3b9500e9b648
mdalzell <matt.a.dalzell@gmail.com>
2019-10-20 20:27:59 -0500

    Added logo and incremented build version

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import { createAppContainer } from 'react-navigation';
 
 import Drawer from './components/shared/Drawer';
+import HomeScreen from './screens/HomeScreen';
 
 const AppContainer = createAppContainer(Drawer);
-const App = () => <AppContainer />;
+const App = () => (__DEV__ ? <AppContainer /> : <HomeScreen />);
 
 export default App;
--- components/shared/NavigationHeader.js
+++ components/shared/NavigationHeader.js
@@ -3,11 +3,12 @@ import { Header, Icon } from 'react-native-elements';
 
 const NavigationHeader = ({ navigation }) => (
   <Header
+    backgroundColor="purple"
     centerComponent={{
       text: 'Loose Squares'.toUpperCase(),
       style: { color: '#fff' },
     }}
-    leftComponent={<Icon color="#fff" name="menu" onPress={navigation.openDrawer} />}
+    leftComponent={__DEV__ && <Icon color="#fff" name="menu" onPress={navigation.openDrawer} />}
   />
 );
 
